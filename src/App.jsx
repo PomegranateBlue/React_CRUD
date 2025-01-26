f759fd8f121bebdeae578072991f60ade984a0bf
bluepome98 <wpdn1998@gmail.com>
2025-01-26 13:51:56 +0900

    국가 추가 시 화면 다운

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,41 @@
 import { useState } from "react";
 import { COUNTRY_LIST } from "./constant.jsx";
 const App = () => {
-  const [medalList, setMedalList] = useState([{ country: "korea" }]);
+  const [medalList, setMedalList] = useState([
+    { country: "korea", goldMedal: 10, silverMedal: 10, copperMedal: 10 },
+  ]);
   const [country, setCountry] = useState("");
   const [goldMedal, setGoldMedal] = useState(0);
   const [silverMedal, setSilverMedal] = useState(0);
   const [copperMedal, setCopperMedal] = useState(0);
+
+  const addMedal = (e) => {
+    e.preventDefault();
+    const newMedal = {
+      country: country,
+      goldMedal: goldMedal,
+      silverMedal: silverMedal,
+      copperMedal: copperMedal,
+    };
+    setMedalList([...medalList, newMedal]);
+  };
 
   return (
     <div>
       <h1>CRUD Practice</h1>
       <div className="formContainer">
         <form>
-          <select value={country} onChange={setCountry}>
-            <option>국가를 선택하세요</option>
+          <select>
+            <option value={country} onChange={setCountry}>
+              국가를 선택하세요
+            </option>
             {COUNTRY_LIST.map((country, index) => (
               <option key={index} value={country}>
                 {country}
               </option>
             ))}
           </select>
+          <label>금메달</label>
           <input
             type="number"
             value={goldMedal}
@@ -28,6 +44,7 @@ const App = () => {
             min="0"
             max="99"
           />
+          <label>은메달</label>
           <input
             type="number"
             value={silverMedal}
@@ -36,6 +53,7 @@ const App = () => {
             min="0"
             max="99"
           />
+          <label>동메달</label>
           <input
             type="number"
             value={copperMedal}
@@ -44,15 +62,35 @@ const App = () => {
             min="0"
             max="99"
           />
-          <button type="submit">ADD</button>
+          <button type="submit" onClick={addMedal}>
+            ADD
+          </button>
           <button type="submit">UPDATE</button>
         </form>
       </div>
-      <div className="MedalTableContainer">
-        {" "}
-        <ul>
-          <li>item</li>
-        </ul>
+      <div className="MedalTable-Container">
+        <table>
+          <thead>
+            <tr className="MedalTable-Header">
+              <th>국가명</th>
+              <th>금메달</th>
+              <th>은메달</th>
+              <th>동메달</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              {medalList.map((medal) => (
+                <tr key={medal.country}>
+                  <td>{medal.country}</td>
+                  <td>{medal.goldMedal}</td>
+                  <td>{medal.silverMedal}</td>
+                  <td>{medal.copperMedal}</td>
+                </tr>
+              ))}
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
